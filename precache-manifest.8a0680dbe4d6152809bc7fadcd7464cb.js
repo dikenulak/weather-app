4b64c11d14255630bff12c43c2d863abe0843545
Diken Ulak <diken@evolveasia.co>
2020-10-13 12:06:18 +0545

    Updates

---
+++ precache-manifest.8a0680dbe4d6152809bc7fadcd7464cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "712d28866a5b85165132bc878f0c8e27",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "b2aa9ca7eb557e7d4c81",
+    "url": "/weather-app/static/css/main.edaa3ff0.chunk.css"
+  },
+  {
+    "revision": "6af5d979d0046cd723dd",
+    "url": "/weather-app/static/js/2.74dd6e22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.74dd6e22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2aa9ca7eb557e7d4c81",
+    "url": "/weather-app/static/js/main.7fdc31c4.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "7f3ac0b6e0f4115d47d26e325a836c04",
+    "url": "/weather-app/static/media/clouds.7f3ac0b6.svg"
+  }
+]);
